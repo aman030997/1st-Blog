d8123272c9abc271fb6d1804c332bb1da5051595
aman030997 <as2026@cse.jgec.ac.in>
2019-10-31 05:29:06 +0530

    add reset password feature

--- Aman/urls.py
+++ Aman/urls.py
@@ -15,12 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from django.urls import path,include
+from django.urls import path,include,re_path
+from django.conf.urls import include, url
 from users import views as user_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/',user_views.register,name='register'),
     path('login/',auth_views.LoginView.as_view(template_name='users/login.html'),name='login'),
     path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'),name='logout'),
-    path('',include('mypoll.urls'))
+    path('',include('mypoll.urls')),
+    
+    
+    re_path(r'^', include('django.contrib.auth.urls')), # for including all auths then we can ignore line 26-30
 ]
